35696b9a63554fc58ae53fd91c4061cc908810e6
Kien <nguyenhoangtrungkien92@gmail.com>
2025-05-11 20:15:34 +0700

    remove baseurl

--- app/src/service/apiService.tsx
+++ app/src/service/apiService.tsx
@@ -1,51 +1,49 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.MODE ? 'http://localhost:5000' : ''
-
 export const fetchPost = async (game: string, platform: string) => {
-    const response = await axios.get(`${baseURL}/${platform}/${game}`)
+    const response = await axios.get(`/${platform}/${game}`)
     return response.data
 }
 
 export const summarize = async (text: string) => {
-    const response = await axios.post(`${baseURL}/summarize`, { text })
+    const response = await axios.post(`/summarize`, { text })
     return response.data
 }
 
 export const getAlldata = async () => {
-    const response = await axios.get(`${baseURL}/alldata`)
+    const response = await axios.get(`/alldata`)
     return response.data
 }
 
 export const getDataOfDate = async (date: string) => {
-    const response = await axios.get(`${baseURL}/data/${date}`)
+    const response = await axios.get(`/data/${date}`)
     return response.data
 }
 
 export const resetData = async () => {
-    const response = await axios.delete(`${baseURL}/alldata`)
+    const response = await axios.delete(`/alldata`)
     return response.data
 }
 
 export const resetDataOfDate = async (date: string) => {
-    const response = await axios.delete(`${baseURL}/data/${date}`)
+    const response = await axios.delete(`/data/${date}`)
     return response.data
 }
 
 export const getAvailableData = async () => {
-    const response = await axios.get(`${baseURL}/availableData`)
+    const response = await axios.get(`/availableData`)
     return response.data
 }
 
 export const downloadAllData = async () => {
-    const response = await axios.get(`${baseURL}/downloadAll`, {
+    const response = await axios.get(`/downloadAll`, {
         responseType: 'blob',
     })
     return response.data
 }
 
 export const downloadDataOfDate = async (date: string) => {
-    const response = await axios.get(`${baseURL}/download/${date}`, {
+    const response = await axios.get(`/download/${date}`, {
         responseType: 'blob',
     })
     return response.data
